f6f39ab80582cabcea6990a28a44222823a74037
root <root@docker.CISCO>
2017-07-05 20:29:25 -0400

    First commit of DICOM scanner.

---
+++ README.md
@@ -0,0 +1,73 @@
+DICOM-Scan - for identifying medical imaging devices
+========
+
+Python-based DICOM scanner -- scans a bunch of computers and tries to determine if its a DICOM medical imaging device.
+
+
+Lesson Learned
+===
+
+DICOM C-language communications generally requires knowledge of the Application Entity Title, but thanks to a library from scaramallion was able to discover that a specific SOP UID could also be used to authenticate.
+Note that despite the common DICOM ports, many DICOM providers mask the ports to something else for security through obscurity purposes.  Also note that the pynetdicom3 library has availability of utilizing UIDs instead
+of actual AET names which is very helpful since most AET are not known and I have not found a known dictionary of them anywhere.
+
+Example:
+
+```bash
+root@docker# ./dicomscanner.py -ip 192.168.10.5
+attempting to scan 192.168.10.5
+root@docker#
+```
+
+```
+root@docker# ./smbscanner.py -ip 192.168.10.5
+attempting to scan 192.168.10.5
+```
+
+In the event that the port is not DICOM, the scanner should attempt a banner grab to try to find out what it is.  Often times, it could be the web wrapper of the DICOM provider in which case you may get a "PDU" output message
+as well as a time out message.
+
+If you use the "-results_file" flag, you should get a nice parseable output:
+
+The file should look like the following:
+
+Dependencies:
+=============
+
+https://github.com/pydicom/pydicom.git
+
+https://github.com/scaramallion/pynetdicom3.git
+
+Usage:
+======
+
+```bash
+./dicomscanner.py -h
+```
+
+DICOM Checker
+  
+Example
+===
+
+```bash
+./dicomscanner.py -ip 192.168.10.5 -port 1104
+
+./dicomscanner.py -netrange 192.168.10.0/24 -port 1104 -results_file results.txt
+```
+
+Bugs
+====
+
+- suppress PDU error message output to a handler
+- suppress and/or redirect a bunch of stdout messages
+
+TODO
+===
+
+- add the abiltity to read target files.
+- maybe later add other C-language DICOM communication types, but for now not going to do it. :)
+If you find other bugs that I haven't mentioned, please report them to gmail (rlastinger) or create a ticket, and I will get to it when I can.  
+
+Help or improvement suggestions are also welcome.  Just email me at gmail (rlastinger).
+Enjoy.
---
+++ dicomscanner.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import argparse
+import io
+import json
+import time
+import netaddr
+import threading
+from socket import *
+from dicom import *
+from pynetdicom3 import AE
+
+def dicomscan(server, port, results_file):
+    print "Attempting DICOM scan on " + '%s' % server + '\n'
+    ts = time.strftime("%Y-%m-%d %H:%M")
+    try:
+        ae = AE(scu_sop_class=['1.2.840.10008.1.1'])
+        peerassoc = ae.associate(server, port)
+        dicom_entry = peerassoc.send_c_echo()
+        peerassoc.release()
+        if '%s' % dicom_entry is not None:
+            if results_file is not None:
+                with print_lock:
+                    with open(results_file, 'a+') as outfile:
+                        dicom_data = 'host: ' + '%s' % server + '\n' + 'is_dicom: true\ndicom_info:' + '%s' % dicom_entry + '\ndicom_port: ' + '%s' % port + '\ntimestamp: ' + '%s' % ts + '\n\n'
+                        outfile.write(dicom_data)
+            else:
+                with print_lock:
+                    print ("[+] " + '%s' % server + ": " + '%s' % dicom_entry + '\n')
+        else:
+            pass
+    except:
+        try:
+            connector = socket(AF_INET, SOCK_STREAM)
+            connector.settimeout(1)
+            connector.connect(('%s' % server, port))
+            connector.send('Friendly Portscanner\r\n')
+            dicom_entry = connector.recv(2048)
+            connector.close()
+            if results_file is not None:
+                with print_lock:
+                    with open(results_file, 'a+') as outfile:
+                       dicom_data = 'host: ' + '%s' % server + '\n' + 'is_dicom: false\ndicom_info:' + '%s' % dicom_entry + '\ndicom_port: ' + '%s' % port + '\ntimestamp: ' + '%s' % ts + '\n\n'
+                       outfile.write(dicom_data)
+            else:
+                with print_lock:
+                    print ("[-] " + '%s' % server + ": " + '%s' % dicom_entry + '\n')
+                    pass
+        except Exception, errorcode:
+            if errorcode[0] == "timed out":
+                print server + ": connection " + errorcode[0] + "\n"
+                pass
+            elif errorcode[0] == "connection refused":
+                print server + ": connection " + errorcode[0] + "\n"
+                pass
+            else:
+                pass
+
+def thread_check(server, results_file):
+    global semaphore
+
+    try:
+        dicomscan(server, port, results_file)
+    except Exception as e:
+        with print_lock:
+           print "[ERROR] [%s] - %s" % (server, e)
+           pass
+    finally:
+        semaphore.release()
+
+if __name__ == "__main__":
+    dicomparser = argparse.ArgumentParser(description="DICOM Scanner")
+    dicomparser.add_argument("-netrange", type=str, required=False, help="CIDR Block")
+    dicomparser.add_argument("-ip", type=str, required=False, help="IP address to scan")
+    dicomparser.add_argument("-port", type=int, required=True, help="Ports to scan for DICOM")
+    dicomparser.add_argument("-results_file", type=str, required=False, help="Results File")
+    dicomparser.add_argument("-packet_rate", default=1, type=int, required=False, help="Packet rate")
+    dicomargs = dicomparser.parse_args()
+
+    semaphore = threading.BoundedSemaphore(value=dicomargs.packet_rate)
+    print_lock = threading.Lock()
+
+    if dicomargs.ip is not None:
+        dicomscan(dicomargs.ip, dicomargs.port, dicomargs.results_file)
+
+    elif dicomargs.netrange is not None:
+       for ip in netaddr.IPNetwork(dicomargs.netrange).iter_hosts():
+           dicomscan(str(ip), dicomargs.port, dicomargs.results_file)
+
+    elif not dicomargs.packet_rate and dicomargs.netrange:
+       for ip in netaddr.IPNetwork(dicomargs.netrange).iter_hosts():
+           semaphore.acquire()
+           dicomthread = threading.Thread(target=thread_check, args=(str(ip), dicomargs.results_file))
+           dicomthread.start()
+           dicomthread.join()
+    else:
+        print "Please provide with either -ip or -netrange.  Or ./dicomscanner.py -h for help.."
+        exit
